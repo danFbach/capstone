c3b829fcb1d95817efe4db44a26b6bfe777bee1a
danFbach <quiktekrepair@gmail.com>
2016-06-07 00:47:22 -0500

    grade book model

---
+++ ClassAnalytics/Models/GradeBookModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClassAnalytics.Models
+{
+    public class GradeBookModel
+    {
+        [Key]
+        public int grade_Id { get; set; }
+
+        [Display(Name = "Student Id")]
+        public int student_Id { get; set; }
+        public StudentModels StudentModels { get; set; }
+        public int task_Id { get; set; }
+        public TaskModel TaskModel { get; set; }
+        [Display(Name = "Notes")]
+        public string assignment_notes { get; set; }
+        [Display(Name = "Grade")]
+        [Range(0,100)]
+        public decimal grade { get; set; }
+    }
+}
